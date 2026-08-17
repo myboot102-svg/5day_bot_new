32d4692fb1c1b6450c2227ebbf4a8050d7d00cce
myboot102-svg <myboot102@gmail.com>
2026-08-17 09:13:30 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,28 @@
+import os
+from telegram import Update
+from telegram.ext import Application, CommandHandler, ContextTypes
 
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text(
+        "هلا بيج بـ 5DAY.\n"
+        "البوت اشتغل بنجاح."
+    )
+
+
+def main():
+    token = os.getenv("BOT_TOKEN")
+
+    if not token:
+        raise ValueError("BOT_TOKEN غير موجود")
+
+    app = Application.builder().token(token).build()
+
+    app.add_handler(CommandHandler("start", start))
+
+    print("Bot is running...")
+    app.run_polling()
+
+
+if __name__ == "__main__":
+    main()
